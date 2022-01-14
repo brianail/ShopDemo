be5d8c2001fbdb12eb9711d4cdd0a0a2a821707c
brianail <80833852+brianail@users.noreply.github.com>
2022-01-13 23:32:36 -0300

    Initial Commit

---
+++ ShopDemo/ShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopDemoApp.swift
+//  ShopDemo
+//
+//  Created by Brian Ail on 13/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
